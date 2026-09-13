96f4bc3ae07e0ed1a1d9a11ec56b3d78229eb903
cfh0714 <cfh07142202@gmail.com>
2026-09-13 02:22:04 +0100

    Add files via upload

---
+++ css-login-design/js/JavaScript.js
@@ -0,0 +1,3 @@
+const usernameInput = document.getElementById("usernamebox");
+const passwordInput = document.getElementById("passwordbox");
+const submitButton = document.getElementById("SubmitButton");
