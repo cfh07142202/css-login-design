0ac99cf9b478de8a1aad54b9ad198ec16ad1fcf7
cfh0714 <cfh07142202@gmail.com>
2026-09-15 20:59:06 +0100

    javaScript.js
    just changed the username and password to a let because you will need to assign it later in the script

--- css-login-design/js/JavaScript.js
+++ css-login-design/js/JavaScript.js
@@ -1,3 +1,3 @@
-const usernameInput = document.getElementById("usernamebox");
-const passwordInput = document.getElementById("passwordbox");
+let usernameInput = document.getElementById("usernamebox");
+let passwordInput = document.getElementById("passwordbox");
 const submitButton = document.getElementById("SubmitButton");
